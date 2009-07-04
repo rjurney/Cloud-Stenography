9073abc10ef91230b14ba5cd3d59d0fa229f46b1
Peyompian <peyomp@russell-jurneys-macbook-pro.local>
2009-07-04 00:59:24 -0400

    First.

---
+++ js/util/inputex/FormContainer-beta.js
@@ -0,0 +1,62 @@
+/**
+ * Class used to build a container with inputEx forms
+ * @class FormContainer
+ * @namespace WireIt
+ * @extends WireIt.Container
+ * @constructor
+ * @param {Object}   options  Configuration object (see properties)
+ * @param {WireIt.Layer}   layer The WireIt.Layer (or subclass) instance that contains this container
+ */
+WireIt.FormContainer = function(options, layer) {
+   WireIt.FormContainer.superclass.constructor.call(this, options, layer);
+};
+
+YAHOO.lang.extend(WireIt.FormContainer, WireIt.Container, {
+   
+   /**
+    * @method setOptions
+    */
+   setOptions: function(options) {
+      WireIt.FormContainer.superclass.setOptions.call(this, options);
+      
+      this.options.legend = options.legend; 
+      this.options.collapsible = options.collapsible; 
+      this.options.fields = options.fields;
+   },
+   
+   /**
+    * The render method is overrided to call renderForm
+    * @method render
+    */
+   render: function() {
+      WireIt.FormContainer.superclass.render.call(this);
+      this.renderForm();
+   },
+   
+   /**
+    * Render the form
+    * @method renderForm
+    */
+   renderForm: function() {
+      for(var i = 0 ; i < this.options.fields.length ; i++) {
+         this.options.fields[i].inputParams.container = this;
+      }
+      var groupParams = {parentEl: this.bodyEl, fields: this.options.fields, legend: this.options.legend, collapsible: this.options.collapsible};
+      this.form = new YAHOO.inputEx.Group(groupParams);
+   },
+   
+   /**
+    * @method getValue
+    */
+   getValue: function() {
+      return this.form.getValue();
+   },
+   
+   /**
+    * @method setValue
+    */
+   setValue: function(val) {
+      this.form.setValue(val);
+   }
+   
+});
